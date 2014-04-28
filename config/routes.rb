99a4c1aa6c62d72a173079ff098096fa1a406e45
abhaverma <abhaverma90@gmail.com>
2014-04-28 12:54:19 +0530

    added new controllers

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,13 @@ FirstApp::Application.routes.draw do
 
   resources :users
 
+  match '/', to: 'users#index', via: 'get'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  # root 'users#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
